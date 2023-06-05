cb9c77d53a5dfee095fa357b92eea3f5dd6b20fe
Dylan Dao <dylanpdao@gmail.com>
2023-06-05 20:40:02 +0000

    added tailwind dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./client/**/*.{ts,tsx}'],
+  content: ['./client/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
