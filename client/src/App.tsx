615cb66c0eff2f84972bda91a083f13b24810bb3
Dylan Dao <dylanpdao@gmail.com>
2023-06-05 21:09:13 +0000

    added primary color

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-600">
+      <h1 className="text-3xl font-bold underline text-primary">
         Simple React Typescript Tailwind Sample
       </h1>
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,html,css}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#B4B4B4',
+      },
+    },
   },
   plugins: [],
 };
