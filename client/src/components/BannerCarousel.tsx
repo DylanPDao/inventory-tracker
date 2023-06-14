be90d9e47cf057d8ac88a8de7771ccad4f484cf6
Dylan Dao <dylanpdao@gmail.com>
2023-06-14 01:49:49 +0000

    added pictures to carousel

--- client/src/components/BannerCarousel.tsx
+++ client/src/components/BannerCarousel.tsx
@@ -1,6 +1,16 @@
 import { Carousel } from '@material-tailwind/react';
 
-export default function BannerCarousel() {
+type Props = {
+  banners: string[];
+};
+
+export default function BannerCarousel({ banners }: Props) {
+  const bannerElem = banners.map((banner: string) => (
+    <>
+      <img src={banner} alt={banner} className="h-full w-full object-cover" />
+    </>
+  ));
+
   return (
     <Carousel
       className="rounded-xl"
@@ -17,21 +27,7 @@ export default function BannerCarousel() {
           ))}
         </div>
       )}>
-      <img
-        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-        alt="image 1"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-        alt="image 2"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-        alt="image 3"
-        className="h-full w-full object-cover"
-      />
+      {bannerElem}
     </Carousel>
   );
 }
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,3 +1,13 @@
+import BannerCarousel from '../components/BannerCarousel';
+
+const banners = [
+  '/images/Banner1.jpeg',
+  '/images/Banner2.jpeg',
+  '/images/Banner4.jpeg',
+  '/images/Banner5.jpeg',
+  '/images/Banner6.jpeg',
+];
+
 export default function LandingPage() {
-  return <></>;
+  return <BannerCarousel banners={banners} />;
 }
