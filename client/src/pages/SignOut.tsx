a7ca6cbd5938e5180c4adda2a085381af3804148
Dylan Dao <dylanpdao@gmail.com>
2023-06-22 21:27:26 +0000

    added signout confirmation page, no setting state and navigating

--- client/src/pages/SignOut.tsx
+++ client/src/pages/SignOut.tsx
@@ -8,6 +8,16 @@ type User = {
 export default function SignOut({ user }: User) {
   const navigate = useNavigate();
   user(undefined);
-  navigate('/');
-  return <></>;
+  return (
+    <div className="container flex justify-center items-center">
+      <div className="mt-12">
+        <p className="mb-4 text-lg">Sign Out Success!</p>
+        <button
+          className="text-white w-6/12 text-xs m-2 p-2 bg-blue-700 hover:bg-blue-800 active:ring-4 rounded-lg text-center"
+          onClick={() => navigate('/')}>
+          Back to home!
+        </button>
+      </div>
+    </div>
+  );
 }
