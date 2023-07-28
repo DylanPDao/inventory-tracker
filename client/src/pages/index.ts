78a73c38f46d3585c95fe544659317a081d204e3
Dylan Dao <dylanpdao@gmail.com>
2023-07-28 18:41:47 +0000

    added orders landing page

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as SignOut } from './SignOut';
 export { default as NotFound } from './NotFound';
 export { default as Inventory } from './Inventory';
 export { default as OrderSheet } from './OrderSheet';
+export { default as Orders } from './Orders';
